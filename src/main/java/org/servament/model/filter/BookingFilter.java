443ec7effccaa88771978be7b4a9408e21322cdc
Luca Corallo <l.corallo2@gmail.com>
2024-07-04 17:27:27 +0200

    adding lombok annotation to missed filter classes

--- src/main/java/org/servament/model/filter/BookingFilter.java
+++ src/main/java/org/servament/model/filter/BookingFilter.java
@@ -5,6 +5,16 @@ import java.util.UUID;
 
 import org.servament.model.BookingStatus;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingFilter {
 
     private List<BookingStatus> statuses;
--- src/main/java/org/servament/model/filter/EventServiceFilter.java
+++ src/main/java/org/servament/model/filter/EventServiceFilter.java
@@ -3,6 +3,17 @@ package org.servament.model.filter;
 import java.util.List;
 import java.util.UUID;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@NoArgsConstructor
+@AllArgsConstructor
+
 public class EventServiceFilter extends EventFIlter {
 
     private List<String> codes;
